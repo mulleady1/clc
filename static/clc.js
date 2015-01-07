5960dd46f3167ec455f697edab57062903757240
Kyle Mulleady <kyle.mulleady@gmail.com>
2015-01-06 20:51:11 -0800

    Loading indicator on search

--- static/clc.js
+++ static/clc.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
     $('#form button').on('click', function(event) {
         event.preventDefault();
+        $('#results').html('Loading...');
         var data = $('#form').serialize();
         $.get('/search?' + data)
         .then(function(results) {
