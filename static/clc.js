b4241830a4cc3f09a355f754c91b7b7a9f6acfcd
mulleady1 <kyle.mulleady@gmail.com>
2015-01-10 14:45:45 -0800

    Forward/back navigation

--- static/clc.js
+++ static/clc.js
@@ -1,13 +1,21 @@
 
 $(document).ready(function() {
     $('button#citysearch').on('click', function(event) {
-        var data = $('#form').serialize(),
+        var data = $('form.citysearch').serialize(),
             url  = '/citysearch?' + data,
             $el  = $('#city-search-results'),
-            $containerEl = $('#form-container');
+            $previousEl = $('#form-container'),
+            $buttonEl   = $(this);
         event.preventDefault();
+        $buttonEl.html('Loading...');
+        $buttonEl.prop('disabled', true);
         $.get(url).then(function(results) {
             $el.html(results);
+            $el.show();
+            $previousEl.hide();
+            $buttonEl.html('Search for cities');
+            $buttonEl.prop('disabled', false);
+
             $('form.jobsearch input[type=checkbox]').each(function() {
                 $(this).on('change', storeCheckedPreference);
                 var city = $(this).val();
@@ -15,29 +23,44 @@ $(document).ready(function() {
                     $(this).prop('checked', true);
                 }
             });
-            $el.show();
-            $containerEl.hide();
             $('a.back-1').on('click', function() {
                 $el.hide();
-                $containerEl.show();
+                $previousEl.show();
+                $forwardEl = $('a.forward-1');
+                $forwardEl.show();
+                $forwardEl.on('click', function() {
+                    $el.show();
+                    $previousEl.hide();
+                });
             });
             $('#checkall').on('change', function() {
                 checkAll($(this).is(':checked'));
             });
-
             $('button#jobsearch').on('click', function(event) {
                 var data = $('form.jobsearch').serialize(),
                     url  = '/jobsearch?' + data,
                     $el  = $('#job-search-results'),
-                    $previousEl = $('#city-search-results');
+                    $previousEl = $('#city-search-results'),
+                    $buttonEl   = $(this);
                 event.preventDefault();
+                $buttonEl.html('Loading...');
+                $buttonEl.prop('disabled', true);
                 $.get(url).then(function(results) {
                     $el.html(results);
                     $el.show();
                     $previousEl.hide();
+                    $buttonEl.html('Search for jobs');
+                    $buttonEl.prop('disabled', false);
+
                     $('a.back-2').on('click', function() {
                         $el.hide();
                         $previousEl.show();
+                        $forwardEl = $('a.forward-2');
+                        $forwardEl.show();
+                        $forwardEl.on('click', function() {
+                            $el.show();
+                            $previousEl.hide();
+                        });
                     });
                 });
             });
